629117339fa227227c2a1ac8d4faf0e11f8d3c5e
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-06-28 11:56:35 +0200

    Remove optrions from msbuild

--- conda/bld.bat
+++ conda/bld.bat
@@ -6,7 +6,7 @@ cmake .. ^
       -DPYTHON_BINDINGS=ON ^
       -DRUN_SWIG=ON ^
 :: cmake --build . --target install
-msbuild /target:install
+msbuild
 
 ::The python library and shared object do not install into site-packages so
 ::we put them there manually after the build.  This may be possible from CMake
