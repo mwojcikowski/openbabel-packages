32145473905b7b3d7089e862c0097bbf15a99fd5
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-06-28 14:31:50 +0200

    fix 13

--- conda/bld.bat
+++ conda/bld.bat
@@ -9,8 +9,6 @@ cmake ^
 
 cmake --build . --target INSTALL -- /verbosity:minimal
 
-if errorlevel 1 exit 1
-
 ::The python library and shared object do not install into site-packages so
 ::we put them there manually after the build.  This may be possible from CMake
 ::using option -DPYTHON_PREFIX from ob wiki, but doesn't seem to work
@@ -24,5 +22,3 @@ rmdir /S %PREFIX%\bin
 :: Install the python site package
 cd scripts\python
 %PYTHON% setup.py install
-
-if errorlevel 1 exit 1
