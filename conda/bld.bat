a04572f1b3caf8973adf94b7a5c817f8be021659
Maciej Wójcikowski <mwojcikowski@users.noreply.github.com>
2016-06-29 23:28:41 +0300

    Update bld.bat

--- conda/bld.bat
+++ conda/bld.bat
@@ -43,9 +43,9 @@ cmake --build . --target install --config Release
 
 
 :: Copy the key binary files to the site packages.  this is an unfortunate workaround for windows
-:: copy bin\_openbabel.pyd %PREFIX%\Lib\site-packages
-:: xcopy %PREFIX%\bin %PREFIX%\Library\bin /E
-:: rmdir /S %PREFIX%\bin
+copy bin\Release\_openbabel.pyd %PREFIX%\Lib\site-packages
+xcopy %PREFIX%\bin\Release %PREFIX%\Library\bin /E
+rmdir /S %PREFIX%\bin\Release
 
 :: Install the python site package
 cd scripts\python
