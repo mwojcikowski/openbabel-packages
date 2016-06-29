3dabf89d140943e87cadb4439b1a43f668c5e7d1
Maciej Wójcikowski <mwojcikowski@users.noreply.github.com>
2016-06-30 01:04:20 +0300

    Update bld.bat

--- conda/bld.bat
+++ conda/bld.bat
@@ -13,14 +13,6 @@ regedit /s x64\656d8766-2429-11d7-8bf6-00b0d03daa06.reg
 copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.Express.config"
 copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.Express.config"
 
-
-:: copy "%PREFIX%\..\..\libs\libpython27.a" "%PREFIX%\libs\libpython27.a"
-:: copy "%PREFIX%\..\..\Lib\distutils\distutils.cfg" "%PREFIX%\Lib\distutils\distutils.cfg"
-
-:: set PATH=C:\\MinGW\\bin;%PATH%
-:: remove git from PATH
-:: set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
-
 cmake ^
       -G "%CMAKE_GENERATOR%" ^
       -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
@@ -34,19 +26,18 @@ cmake ^
 cmake --build . --target install --config Release 
 ::-- /msbuild:m /msbuild:logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll"
 
-:: mingw32-make -j4
-:: mingw32-make install
-
 ::The python library and shared object do not install into site-packages so
 ::we put them there manually after the build.  This may be possible from CMake
 ::using option -DPYTHON_PREFIX from ob wiki, but doesn't seem to work
 
-
 :: Copy the key binary files to the site packages.  this is an unfortunate workaround for windows
 copy bin\Release\_openbabel.pyd %PREFIX%\Lib\site-packages
+copy bin\Release\_openbabel.dll %PREFIX%\Lib\site-packages
+copy scripts\python\openbabel.py %PREFIX%\Lib\site-packages
+copy scripts\python\pybel.py %PREFIX%\Lib\site-packages
 xcopy %PREFIX%\Release\bin %PREFIX%\Library\bin /E
 ::rmdir /S %PREFIX%\Release\bin
 
 :: Install the python site package
-cd scripts\python
-%PYTHON% setup.py install
+::cd scripts\python
+::%PYTHON% setup.py install
