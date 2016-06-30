6f141c273fed305f4ee16d26bf8ae22ffe8cd816
Maciek Wójcikowski <maciek@wojcikowski.pl>
2016-06-30 11:59:34 +0200

    Fix installing 2

--- conda/bld.bat
+++ conda/bld.bat
@@ -1,17 +1,17 @@
 :: patch MSVS 2008 64bit
-regedit /s x64\VC_OBJECTS_PLATFORM_INFO.reg
+::regedit /s x64\VC_OBJECTS_PLATFORM_INFO.reg
 
-regedit /s x64\600dd186-2429-11d7-8bf6-00b0d03daa06.reg
-regedit /s x64\600dd187-2429-11d7-8bf6-00b0d03daa06.reg
-regedit /s x64\600dd188-2429-11d7-8bf6-00b0d03daa06.reg
-regedit /s x64\600dd189-2429-11d7-8bf6-00b0d03daa06.reg
-regedit /s x64\656d875f-2429-11d7-8bf6-00b0d03daa06.reg
-regedit /s x64\656d8760-2429-11d7-8bf6-00b0d03daa06.reg
-regedit /s x64\656d8763-2429-11d7-8bf6-00b0d03daa06.reg
-regedit /s x64\656d8766-2429-11d7-8bf6-00b0d03daa06.reg
+::regedit /s x64\600dd186-2429-11d7-8bf6-00b0d03daa06.reg
+::regedit /s x64\600dd187-2429-11d7-8bf6-00b0d03daa06.reg
+::regedit /s x64\600dd188-2429-11d7-8bf6-00b0d03daa06.reg
+::regedit /s x64\600dd189-2429-11d7-8bf6-00b0d03daa06.reg
+::regedit /s x64\656d875f-2429-11d7-8bf6-00b0d03daa06.reg
+::regedit /s x64\656d8760-2429-11d7-8bf6-00b0d03daa06.reg
+::regedit /s x64\656d8763-2429-11d7-8bf6-00b0d03daa06.reg
+::regedit /s x64\656d8766-2429-11d7-8bf6-00b0d03daa06.reg
 
-copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.Express.config"
-copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.Express.config"
+::copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\AMD64.VCPlatform.Express.config"
+::copy "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.config" "C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcpackages\Itanium.VCPlatform.Express.config"
 
 cmake ^
       -G "%CMAKE_GENERATOR%" ^
@@ -32,12 +32,13 @@ cmake --build . --target install --config Release
 
 :: Copy the key binary files to the site packages.  this is an unfortunate workaround for windows
 copy bin\Release\_openbabel.pyd %PREFIX%\Lib\site-packages
-::copy bin\Release\_openbabel.dll %PREFIX%\Lib\site-packages
+copy scripts\Release\_openbabel.dll %PREFIX%\Lib\site-packages
+copy scripts/Release/_openbabel.lib %PREFIX%\Lib\site-packages
 copy scripts\python\openbabel.py %PREFIX%\Lib\site-packages
 copy scripts\python\pybel.py %PREFIX%\Lib\site-packages
 xcopy %PREFIX%\bin %PREFIX%\Library\bin /e /c
 rmdir /s /q %PREFIX%\bin
 
 :: Install the python site package
-cd scripts\python
-%PYTHON% setup.py install
+::cd scripts\python
+::%PYTHON% setup.py install
