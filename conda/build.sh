3dabd23c36f5681abf7e59e59bbae453a7aab918
Maciek Wojcikowski <maciek@wojcikowski.pl>
2016-09-26 19:32:28 +0200

    Enhance build script

--- conda/build.sh
+++ conda/build.sh
@@ -5,10 +5,20 @@ else
     SO_EXT='so'
 fi
 
+PYTHON_INCLUDE_DIR=$($PYTHON -c 'import distutils.sysconfig, sys; sys.stdout.write(distutils.sysconfig.get_python_inc())')
+PYTHON_LIBRARY=$($PYTHON -c 'from distutils.sysconfig import get_config_var; import os, sys; sys.stdout.write(os.path.join(get_config_var("LIBDIR"),get_config_var("LDLIBRARY")))')
+
 cmake -DCMAKE_INSTALL_PREFIX=$PREFIX \
-      -DOPTIMIZE_NATIVE=OFF \
+      -DPYTHON_LIBRARY=$PYTHON_LIBRARY \
+      -DPYTHON_EXECUTABLE=$PYTHON \
+      -DPYTHON_INCLUDE_DIR=$PYTHON_INCLUDE_DIR \
       -DPYTHON_BINDINGS=ON \
+      -DLIBXML2_INCLUDE_DIR=$PREFIX/include \
+      -DLIBXML2_LIBRARIES=$PREFIX/lib/libxml2.${SO_EXT} \
+      -DZLIB_INCLUDE_DIR=$PREFIX/include \
+      -DZLIB_LIBRARY=$PREFIX/lib/libz.${SO_EXT} \
       -DRUN_SWIG=ON
+
 
 make -j${CPU_COUNT}
 make install
